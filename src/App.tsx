e77f61d4f505a457918e91ea0d0043741e7b1e57
Nikko Solon <nikkosolon24@gmail.com>
2023-06-12 09:09:54 -0700

    added: wicked loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import './App.css'
+import styled, { keyframes } from 'styled-components'
 
 function App() {
   return (
-    <>
+    <div>
+      <Spinner></Spinner>
       <h1>Failed successfully</h1>
-    </>
+    </div>
   )
 }
 
 export default App
+
+const AnimateSpin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const Spinner = styled.div`
+    display: inline-block;
+    height: 2em;
+    width: 3em;
+    border: 10px solid;
+    border-top-color: #4d2aff;
+    border-bottom-color: #ff462a;
+    border-left-color: #2aff5c;
+    border-right-color: #fff82a;
+    border-radius: 50%;
+    animation: ${AnimateSpin} 0.00002s linear infinite;
+`
