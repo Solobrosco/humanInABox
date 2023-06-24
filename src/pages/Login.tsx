7a90840b04b8ad096fcf026c384d9d73f25db38b
Nikko Solon <nikkosolon24@gmail.com>
2023-06-23 21:49:36 -0700

    Created: Login.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { styled } from "styled-components"
+
+const Login = () => {
+    return(
+        <div>
+            <Container>Cont</Container>
+        </div>
+    )
+}
+
+export default Login
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+    background-color: grey;
+`
