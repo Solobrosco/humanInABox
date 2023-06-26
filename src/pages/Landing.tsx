07a94ca2f9985ecc06a1ab6475113df80511739f
Solobrosco <nikkosolon24@gmail.com>
2023-06-26 08:40:01 -0700

    Created: Landing.tsx

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,14 @@
+import Spinner from "../components/Spinner"
+// import Login from "./Login"
+
+const Landing = () => {
+    return(
+        <div>
+            <h1>Failed successfully</h1>
+            <Spinner />
+            {/* <button onSubmit={Login}>Login</button> */}
+        </div>
+    )
+}
+
+export default Landing
