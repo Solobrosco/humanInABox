fbfd33b0cb190e2c04aa112bdc1255c2c5775429
Solobrosco <nikkosolon24@gmail.com>
2023-07-03 17:21:32 -0700

    Linked: login endpoint to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,48 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom"
+// import { Routes, Route, BrowserRouter } from "react-router-dom"
 
 // import Spinner from './components/Spinner'
 // import Login from './pages/Login'
 // import DVDLoading from './components/DvdLoading'
 import Landing from "./pages/Landing"
-// import Login from './pages/Login'
 
 import './App.css'
+import { styled } from "styled-components"
+import { Link } from "react-router-dom"
 
 
 function App() {
   return (
-      // <Routes>
-      //   <Route path="/" element={ <Landing /> } />
-      //   <Route path="/login" element={ <Login /> } />
-      // </Routes>
+    <>
       <Landing />
+        <Link to = {'login/'}>
+          <Button>
+            Login
+          </Button>
+        </Link>
+    </>
   )
 }
 
 export default App
+
+const Button = styled.div`
+  margin-top: 1em;
+  color: white;
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  transition: all 0.2s ease;
+  &:hover {
+    color: #c9c9c9;
+    background-color: #0e0e0e;
+  }
+  &:focus, &:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+`
