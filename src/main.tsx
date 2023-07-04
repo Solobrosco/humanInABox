153d03ff4148375b6d990651b4ee64a40a76193b
Solobrosco <nikkosolon24@gmail.com>
2023-07-03 17:20:46 -0700

    Created: router in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './pages/Login.tsx'
 
-const root = ReactDOM.createRoot(document.getElementById("root")as HTMLElement)
+const router = createBrowserRouter ([
+  {
+    path: 'humanInABox/',
+    element: <App />
+  },
+  {
+    path: 'humanInABox/login',
+    element: <Login />
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <RouterProvider router={router} />
 )
