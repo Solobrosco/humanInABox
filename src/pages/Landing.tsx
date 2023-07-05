45662f7819346c06fc6a3fa36e51e7a5de081f81
Solobrosco <nikkosolon24@gmail.com>
2023-07-05 14:57:01 -0700

    Refactor: Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,41 @@
+import { Link } from "react-router-dom"
+import { styled } from "styled-components"
 import Spinner from "../components/Spinner"
-// import Login from "./Login"
 
 const Landing = () => {
     return(
         <div>
             <h1>Failed successfully</h1>
             <Spinner />
-            {/* <button onSubmit={Login}>Login</button> */}
+            <Link to = {'login/'}>
+                <Button>
+                    Login
+                </Button>
+            </Link>
         </div>
     )
 }
 
 export default Landing
+
+const Button = styled.div`
+  margin-top: 1em;
+  color: white;
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  transition: all 0.2s ease;
+  &:hover {
+    color: #c9c9c9;
+    background-color: #0e0e0e;
+  }
+  &:focus, &:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+`
