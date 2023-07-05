37cc59d4bd199f17a0ed1e61fcbc4eb5bd563d6d
Solobrosco <nikkosolon24@gmail.com>
2023-07-05 14:53:22 -0700

    Refactor: App.tsx & main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import Login from './pages/Login.tsx'
+import App from './App.tsx'
+
+import './index.css'
 
 const router = createBrowserRouter ([
   {
