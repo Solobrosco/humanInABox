07df3bf913d9a4682442f937fd86e66c75877b4b
Solobrosco <nikkosolon24@gmail.com>
2023-07-12 23:57:23 -0700

    Updated: Quote.tsx

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -18,9 +18,12 @@ export default Quote
 
 const Display = styled.div`
     font-size: 5rem;
+    text-shadow: 5px -4px 4px rgb(0, 0, 0);
 `
 
 const Author  = styled.div`
     font-size: 1rem;
     font-weight: thin;
+    color: rgb(0, 140, 255);
+    text-shadow: 2px -2px 1.8px rgb(0, 0, 0);
 `
