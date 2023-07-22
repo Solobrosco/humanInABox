dbf42cca9de898fb3c7b899f2f6ee1b04704af92
Solobrosco <nikkosolon24@gmail.com>
2023-07-21 23:05:59 -0700

    Created Gradient.tsx

---
+++ src/components/Gradient.tsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components"
+
+const Gradient = () =>{
+    return (
+      <div>Gradient</div>
+    )
+}
+
+export default Gradient
+
+const Grade = styled.div`
+    
+`
