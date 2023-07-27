c95bfc3ef8239133b97a8ee6b469b4719f4071cd
Solobrosco <nikkosolon24@gmail.com>
2023-07-26 22:42:08 -0700

    Updated: Gradient.tsx

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -1,20 +1,29 @@
 import { styled } from "styled-components"
 
 const Gradient = () =>{
-    return (
+  return (
+    <Container>
       <Grade/>
-    )
+    </Container>
+  )
 }
 
 export default Gradient
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3rem;
+`
+
 const Grade = styled.div`
-    
-    height: 5rem;
-    width: 5rem;
-    background: linear-gradient(
-    20deg,
-    hsl(340, 60%, 65%),
-    hsl(${340 - 305}, 64%, 60%)
+  height: 5rem;
+  width: 10rem;
+  background: linear-gradient(
+  20deg,
+  hsl(340, 60%, 65%),
+  hsl(${340 - 305}, 64%, 40%),
+  hsl(${340 - 200}, 30%, 50%)
   );
 `
