9b0f44a68522ce5bcdab9eceb12a5d007ed826a8
Solobrosco <nikkosolon24@gmail.com>
2023-08-01 03:58:42 -0700

    Updated: Gradient.tsx

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -1,4 +1,4 @@
-import { styled } from "styled-components"
+import { keyframes, styled } from "styled-components"
 
 const Gradient = () =>{
   return (
@@ -10,20 +10,30 @@ const Gradient = () =>{
 
 export default Gradient
 
+const Animate = keyframes`
+  0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+`;
+
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-top: 1rem;
+  padding-top: 3rem;
 `
 
 const Grade = styled.div`
-  height: 5rem;
+  height: 10rem;
   width: 10rem;
-  background: linear-gradient(
-  20deg,
-  hsl(340, 60%, 65%),
-  hsl(${340 - 305}, 64%, 40%),
-  hsl(${340 - 200}, 30%, 50%)
-  );
+  background: linear-gradient(-45deg, #424242, #c90000, #000000);
+	animation: ${Animate} 18s ease infinite;
+  background-size: 600% 400%;
+  border-radius: 25%;
 `
