724c3a72f693af8b3d237c1da93d2bc25bb5af36
Solobrosco <nikkosolon24@gmail.com>
2023-08-11 23:25:20 -0700

    Updated: TodoForm.tsx

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { styled } from 'styled-components'
 
 export const TodoForm = () => {
     const [value, setValue] = useState("")
@@ -8,9 +9,31 @@ export const TodoForm = () => {
         console.log(value)
     }
   return (
-    <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='What is your task?'onChange={(e) => setValue(e.target.value)}/>
-        <button type='submit'>Add Task</button>
-    </form>
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledInput type='text' placeholder='What is your task?' onChange={(e) => setValue(e.target.value)}/>
+      <StyledButton type='submit'>Add Task</StyledButton>
+      {/* <input type='text' placeholder='What is your task?'onChange={(e) => setValue(e.target.value)}/> */}
+      {/* <button type='submit'>Add Task</button> */}
+    </StyledForm>
   )
 }
+
+const StyledForm = styled.form`
+  width: 100%;
+  max-width: 700px;
+  padding: 1rem;
+  background-color: #ffd29a;
+  border-radius: 10px;
+  box-sizing: border-box;
+`
+
+const StyledInput = styled.input`
+  display: inline;
+  width: 100%;
+  height: 30px;
+  border-radius: 10px;
+`
+
+const StyledButton = styled.button`
+  display: inline;
+`
